40a2a93bf7c97e463136aac8f8207e996ba0b309
KEVIN MEHTA <kevinmehta137@gmail.com>
2018-11-07 00:43:53 -0800

    heroku deply

---
+++ client/build/precache-manifest.94f5316596e486fe3d9951407ad700cd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7389f6e97bc90b4f4bdbd1bd93f7b7c",
+    "url": "/static/media/temp.b7389f6e.jpg"
+  },
+  {
+    "revision": "6d047e7bc56d9f966adbad6e297c3964",
+    "url": "/static/media/log1.6d047e7b.png"
+  },
+  {
+    "revision": "1387c6aa81462e69f3042054cd97aa1c",
+    "url": "/static/media/l.1387c6aa.png"
+  },
+  {
+    "revision": "f106e45b9237a205086db58c03e40d91",
+    "url": "/static/media/OB82AT0.f106e45b.jpg"
+  },
+  {
+    "revision": "5333b0e08576410457df3704e8824953",
+    "url": "/static/media/Flat design illustration your design icons for online.5333b0e0.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0971e531caf0af54869a",
+    "url": "/static/js/main.0971e531.chunk.js"
+  },
+  {
+    "revision": "87906195479ab65366cb",
+    "url": "/static/js/1.87906195.chunk.js"
+  },
+  {
+    "revision": "0971e531caf0af54869a",
+    "url": "/static/css/main.33f70249.chunk.css"
+  },
+  {
+    "revision": "29e1b40a80812606592d347630577dd7",
+    "url": "/index.html"
+  }
+];
